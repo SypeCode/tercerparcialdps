89155b0c16c37de373a7270647cf39d3ddd8c650
luna2197 <robertoluna_37@hotmail.com>
2020-11-09 16:28:41 -0600

    Formulario y lista de  empleados creada

--- src/Components/Home/Empleados.jsx
+++ src/Components/Home/Empleados.jsx
@@ -2,7 +2,57 @@ import React from "react"
 
 const Empleados = () => (
   <React.Fragment>
-    <h3 class="text-center italic">Aqui va el registro</h3>
+    <div class="container my-5 py-5 z-depth-1">
+
+
+<section class="px-md-5 mx-md-5 text-center text-lg-left dark-grey-text">
+
+
+  
+  <div class="row d-flex justify-content-center">
+
+    
+    <div class="col-md-6">
+
+      
+      <form class="text-center" action="">
+
+        <p class="h4 mb-4">Datos del empleado</p>
+
+        <div class="form-row mb-4">
+          <div class="col">           
+          <input type="text" id="id" class="form-control" placeholder="ID empleado"/>
+          </div>
+          <div class="col">
+            
+            <input type="text" id="nombre" class="form-control" placeholder="Nombre Empleado"/>
+            </div>
+        </div>
+
+        
+        <input type="text" id="horas" class="form-control mb-4" placeholder="Horas Trabajadas"/>
+
+        
+        <button class="btn btn-info my-4 btn-block" type="submit">Registrar</button>
+
+        
+       
+
+      </form>
+      
+    </div>
+    
+
+  </div>
+  
+
+
+</section>
+
+
+
+</div>
+
   </React.Fragment>
 )
 export default Empleados
--- src/Components/Home/Planilla.jsx
+++ src/Components/Home/Planilla.jsx
@@ -2,7 +2,73 @@ import React from "react"
 
 const Planilla = () => (
   <React.Fragment>
-    <h3 class="text-center italic">Aqui va la planilla</h3>
+    <div class="container my-5">
+
+
+<section class="dark-grey-text text-center mb-5">
+
+  
+  <h3 class="font-weight-bold mb-5">Lista Empleados</h3>
+  
+    
+  <div class="card">
+    <div class="card-body">
+      <table class="table table-responsive-md mb-0">
+        <thead>
+          <tr>
+            <th>
+              <strong>Codigo Empleado</strong>
+            </th>
+            <th>
+              <strong>Nombre Empleado</strong>
+            </th>
+            <th>
+              <strong>Horas</strong>
+            </th>
+            <th>
+              <strong>ISSS</strong>
+            </th>
+            <th>
+              <strong>AFP</strong>
+            </th>
+            <th>
+              <strong>Renta</strong>
+            </th>
+            <th>
+              <strong>Sueldo L</strong>
+            </th>
+            <th>
+              <strong>Sueldo Neto</strong>
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">045</th>
+            <td>Alex</td>
+            <td>80</td>
+            <td>$10</td>
+            <td>$20</td>
+            <td>$30</td>
+            <td>$500</td>
+            <td>$440</td>
+          </tr>
+          <tr>
+            
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+  
+
+  </section>
+
+
+
+</div>
+
+
   </React.Fragment>
 )
 export default Planilla
